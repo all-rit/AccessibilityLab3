5871344fbe51a4ee6df6a4ed0163b3205de611c3
Parth Sane <laerdevstudios@gmail.com>
2019-07-10 10:37:20 -0400

    Added changes for Lab3

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
-
+import catImage from "../assets/images/c1.svg";
+import carImage from "../assets/images/c2.svg";
+import burgerImage from "../assets/images/b.png";
 class Game extends Component {
 	render() {
+		const imgStyle = {width:"128px",height:"128px",border:"1px solid black"};
+		const tableStyle = {border: "1px solid black"};
 		return (
 			<div>
-				<table>
+				<p>Click on the image of a cat.</p>
+				<table style={tableStyle}>
 					<tr>
-						<td>1</td>
-						<td>2</td>
+						<td><input style={imgStyle} type={"image"} src={catImage}/></td>
+						<td><input style={imgStyle} type={"image"} src={carImage}/></td>
 					</tr>
 					<tr>
-						<td>3</td>
-						<td>4</td>
+						<td><input style={imgStyle} type={"image"} src={burgerImage}/></td>
+						<td><input style={imgStyle} type={"image"} src={catImage}/></td>
 					</tr>
 				</table>
 			</div>
