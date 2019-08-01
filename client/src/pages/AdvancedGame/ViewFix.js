9d47049cd679c9dd6ac99bce040f990c186645b7
Parth Sane <laerdevstudios@gmail.com>
2019-08-01 09:39:11 -0400

    Removed unused import Link from ViewFix.js

--- client/src/pages/AdvancedGame/ViewFix.js
+++ client/src/pages/AdvancedGame/ViewFix.js
@@ -1,5 +1,5 @@
 import React,{Component} from "react";
-import {Link, Typography} from "@material-ui/core";
+import {Typography} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import {navigate} from "@reach/router";
 class ViewFix extends Component{
