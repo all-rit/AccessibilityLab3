aafad9c7b5993cc448d09097b403a7298b1d96b6
Parth Sane <laerdevstudios@gmail.com>
2019-08-06 14:05:47 -0400

    Fixed typo error

--- client/src/pages/AdvancedGame/AdvancedGame.js
+++ client/src/pages/AdvancedGame/AdvancedGame.js
@@ -13,7 +13,7 @@ class AdvancedGame extends Component {
                 <Typography aria-label={"instructions"} variant={"body1"} gutterBottom>The learning
                     objective of this lab is for students to learn and apply the Understandable
                     accessibility principle. The game consists of performing
-                    some tasks. CLick Start game to begin!</Typography><br/>
+                    some tasks. Click Start game to begin!</Typography><br/>
                 <Button href={"/Lab3/AdvancedInstructions"} variant={"contained"} color={"primary"}
                         aria-label={"Start Game"}>
                     Start Game
