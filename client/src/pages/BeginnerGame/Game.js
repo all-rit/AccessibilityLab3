019f864d31abd238b836daa5395a689f6ef82c39
Parth Sane <laerdevstudios@gmail.com>
2019-09-30 10:46:11 -0400

    Reformatted document for accessibility and code readability

--- client/src/pages/BeginnerGame/Game.js
+++ client/src/pages/BeginnerGame/Game.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import catImage from "../../assets/images/c1.svg";
 import carImage from "../../assets/images/c2.svg";
 import burgerImage from "../../assets/images/b.png";
 import CatClickNavigate from '../../components/helpers/CatClickNavigate.js'
-import {Typography} from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import StickyFooter from "../../components/helpers/StickyFooter";
@@ -11,19 +11,19 @@ import StickyFooter from "../../components/helpers/StickyFooter";
 class Game extends Component {
     constructor(props) {
         super(props);
-        this.state = {render: ''};
+        this.state = { render: '' };
     }
 
     _renderSubComp() {
         if (this.state.render === 'CatClickNavigate') {
-            return <CatClickNavigate path={"/Lab3/GameInstructions"}/>
+            return <CatClickNavigate path={"/Lab3/GameInstructions"} />
         }
     }
 
     render() {
         const catClick = () => {
             console.log('Cat image clicked!');
-            this.setState({render: 'CatClickNavigate'});
+            this.setState({ render: 'CatClickNavigate' });
         };
         const burgerClick = () => {
             console.log('Burger image clicked!');
@@ -31,38 +31,46 @@ class Game extends Component {
         const carClick = () => {
             console.log('Car image clicked!');
         };
-        const appBarStyle = {flexGrow: "1"};
-        const imgStyle = {width: "128px", height: "128px", border: "1px solid black"};
-        const tableStyle = {border: "1px solid black", marginLeft: "auto", marginRight: "auto", textAlign: "center"};
+        const appBarStyle = { flexGrow: "1" };
+        const imgStyle = { width: "128px", height: "128px", border: "1px solid black" };
+        const tableStyle = { border: "1px solid black", marginLeft: "auto", marginRight: "auto", textAlign: "center" };
         return (
             <div>
                 <AppBar position="static" color={"primary"} style={appBarStyle}>
                     <Toolbar>
                         <Typography variant={"h4"} aria-label={"Click on the image of a cat"}
-                                    color={"inherit"}>
+                            color={"inherit"}>
                             Click on the image of a cat.
                         </Typography>
                     </Toolbar>
                 </AppBar>
-                <br/>
+                <br />
                 <table style={tableStyle} className={"center"}>
                     <tbody>
-                    <tr>
-                        <td><input style={imgStyle} type={"image"} src={catImage} onClick={() => catClick()} alt={""}/>
-                        </td>
-                        <td><input style={imgStyle} type={"image"} src={carImage} onClick={() => carClick()} alt={""}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><input style={imgStyle} type={"image"} src={burgerImage} onClick={() => burgerClick()}
-                                   alt={""}/></td>
-                        <td><input style={imgStyle} type={"image"} src={catImage} onClick={() => catClick()} alt={""}/>
-                        </td>
-                    </tr>
+                        <tr>
+                            <td>
+                                <button style={imgStyle} onClick={() => catClick()} alt={""}>
+                                    <img src={catImage} />
+                                </button>
+                            </td>
+                            <td>
+                                <button style={imgStyle} onClick={() => carClick()} alt={""} >
+                                    <img src={carImage} />
+                                </button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <button style={imgStyle} onClick={() => burgerClick()} alt={""}><img src={burgerImage} /></button>
+                            </td>
+                            <td>
+                                <button style={imgStyle} onClick={() => catClick()} alt={""}><img src={catImage} /></button>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
                 {this._renderSubComp()}
-                <StickyFooter/>
+                <StickyFooter />
             </div>
 
         );
