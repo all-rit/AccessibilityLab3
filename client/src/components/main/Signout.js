771faf8a93e1caac1254e82aee6a079dfd8eb881
Parth Sane <laerdevstudios@gmail.com>
2019-11-09 09:16:58 -0500

    Resolved cors issue

---
+++ client/src/components/main/Signout.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../../assets/stylesheets/signout.css';
+
+/*
+Function for handling the signout of a user
+*/
+const handleSignout = () => {
+    fetch('http://all.rit.edu/Lab3/'+process.env.REACT_APP_SERVER_URL + '/logout', {
+        method: 'GET',
+        credentials:'include',
+        headers:{'Access-Control-Allow-Origin':'*', 'Access-Control-Allow-Headers': 'X-Requested-With'}
+    })
+        .then(res => res.text())
+        .then(() => window.location.href = 'http://all.rit.edu/Lab3/')
+        .then(() => {
+            this.setState({'user': null,'loginEnabled': false})
+        })
+        .catch(error => console.log(error))
+};
+
+/*
+Component for the dropdown below a users name on the application
+*/
+const Signout = ({user}) => {
+
+    return (
+        <div className='dropdown signinButton'>
+            <p className='username'>Welcome, {user}! &#9662;</p>
+            <div className='dropdown-content'>
+                <button onClick={() => handleSignout()} className='link'>
+                    Signout
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Signout;
--- client/src/components/main/WelcomeMessage.js
+++ client/src/components/main/WelcomeMessage.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
 
 import LoginButton from './LoginButton';
+import Signout from "./Signout";
 
 class WelcomeMessage extends Component {
     render() {
         const {user, loginEnabled} = this.props;
-
         if (user === null || user.firstname === null) {
             return <LoginButton enabled={loginEnabled}/>;
         }
 
-        return <span>Welcome, {user.firstname}!</span>;
+        return <Signout user={user.firstname}/>;
+
+
     }
 }
 
