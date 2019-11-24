48205ed2f8d1fe0c5221da92387e01866360f117
Parth Sane <laerdevstudios@gmail.com>
2019-11-23 23:29:24 -0500

    Revert "removed unnecessary files"
    This reverts commit 12f277aeb1ad1d4ff8b8ed395c14c6df83e378d2.

---
+++ client/src/components/main/AppInstructions.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class AppInstructions extends Component {
+    render() {
+
+        return <p className="app__instructions">Choose appropriate
+            difficulty to begin game!</p>;
+    }
+}
+
+export default AppInstructions;
