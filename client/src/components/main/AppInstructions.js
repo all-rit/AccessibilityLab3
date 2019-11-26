1174afaf1881634d2056dc88a9d77907cee8b134
Parth Sane <laerdevstudios@gmail.com>
2019-11-25 22:53:07 -0500

    Updated app instructions

--- client/src/components/main/AppInstructions.js
+++ client/src/components/main/AppInstructions.js
@@ -1,10 +1,12 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 
 class AppInstructions extends Component {
     render() {
 
-        return <p className="app__instructions">Choose appropriate
-            difficulty to begin game!</p>;
+        return <Fragment>
+            <p className="app__instructions" tabIndex={0}>Choose appropriate
+            difficulty to begin game! Note: This activity requires headphones or speakers.</p>
+        </Fragment>;
     }
 }
 
